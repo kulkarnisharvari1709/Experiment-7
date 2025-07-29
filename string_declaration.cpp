b36aa888695c6b00a3cc0fc2c94c2990184f0117
kulkarnisharvari1709 <kulkarnisharvari1709@gmail.com>
2025-07-29 15:01:04 +0530

    Create string_declaration.cpp

---
+++ string_declaration.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string s1 = "Hello";
+    string s2("World");
+    char s3[] = "C++";
+
+    cout << "String 1: " << s1 << endl;
+    cout << "String 2: " << s2 << endl;
+    cout << "String 3: " << s3 << endl;
+
+    return 0;
+}
